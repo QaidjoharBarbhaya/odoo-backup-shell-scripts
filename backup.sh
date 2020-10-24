e25bd252c14f4a8df0f67e11a062acff3a7d20e7
Qaidjohar Barbhaya <qaidjoharbarbhaya@gmail.com>
2020-10-24 16:51:47 +0530

    [IMP] update script as per non docker Odoo deployment method

--- backup.sh
+++ backup.sh
@@ -1,14 +1,26 @@
+#cd /home/backup/
+#docker exec -it db pg_dump -U odoo dbname > dbname.sql
+#tar czvf "db_"`date +"%Y-%m-%d"`".tar.gz" dbname.sql
+#rm dbname.sql
+#rm "db_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
+#docker exec -it --user root odoo tar -C /var/lib/odoo/filestore -czpvf /var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" dbname 
+#docker exec -it --user root odoo rm /var/lib/odoo/filestore/"filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
+#docker cp odoo:/var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" .
+#rm "filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz" 
+#../dropbox_uploader.sh upload "db_"`date +"%Y-%m-%d"`".tar.gz" /
+#../dropbox_uploader.sh delete /"db_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
+#../dropbox_uploader.sh upload "filestore_"`date +"%Y-%m-%d"`".tar.gz" /
+#../dropbox_uploader.sh delete /"filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
+
 cd /home/backup/
-docker exec -it db pg_dump -U odoo dbname > dbname.sql
+pg_dump dbname -U odoo > soussa.sql
 tar czvf "db_"`date +"%Y-%m-%d"`".tar.gz" dbname.sql
 rm dbname.sql
 rm "db_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
-docker exec -it --user root odoo tar -C /var/lib/odoo/filestore -czpvf /var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" dbname 
-docker exec -it --user root odoo rm /var/lib/odoo/filestore/"filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
-docker cp odoo:/var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" .
-rm "filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz" 
 ../dropbox_uploader.sh upload "db_"`date +"%Y-%m-%d"`".tar.gz" /
 ../dropbox_uploader.sh delete /"db_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
+tar -C /var/lib/odoo/filestore -czpvf "filestore_"`date +"%Y-%m-%d"`".tar.gz" dbname
+rm "filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
 ../dropbox_uploader.sh upload "filestore_"`date +"%Y-%m-%d"`".tar.gz" /
 ../dropbox_uploader.sh delete /"filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
 
