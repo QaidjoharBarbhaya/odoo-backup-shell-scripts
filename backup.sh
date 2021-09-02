769b63decf113fa305d7ba8a043229b3c6525a75
Qaidjohar Barbhaya <qaidjoharbarbhaya@gmail.com>
2021-09-02 11:40:25 +0530

    [FIX] remove -it, possible cronjob break

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #cd /home/backup/
-#docker exec -it db pg_dump -U odoo dbname > dbname.sql
+#docker exec db pg_dump -U odoo dbname > dbname.sql
 #tar czvf "db_"`date +"%Y-%m-%d"`".tar.gz" dbname.sql
 #rm dbname.sql
 #rm "db_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
-#docker exec -it --user root odoo tar -C /var/lib/odoo/filestore -czpvf /var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" dbname 
-#docker exec -it --user root odoo rm /var/lib/odoo/filestore/"filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
+#docker exec --user root odoo tar -C /var/lib/odoo/filestore -czpvf /var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" dbname 
+#docker exec --user root odoo rm /var/lib/odoo/filestore/"filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz"
 #docker cp odoo:/var/lib/odoo/filestore/"filestore_"`date +"%Y-%m-%d"`".tar.gz" .
 #rm "filestore_"`date --date="1 day ago" +"%Y-%m-%d"`".tar.gz" 
 #../dropbox_uploader.sh upload "db_"`date +"%Y-%m-%d"`".tar.gz" /
