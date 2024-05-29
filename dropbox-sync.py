ebf8dcbbe17023181111c52ef85c25b58a10be80
Qaidjohar Barbhaya <qaidjoharbarbhaya@gmail.com>
2024-05-29 11:15:49 +0530

    [FIX] ensure files get uploaded if fail in between
    Files which are 30+ GB arbitrarily might fail due to invalid offset error

--- dropbox-sync.py
+++ dropbox-sync.py
@@ -25,28 +25,35 @@ try:
 except ApiError:
     pass
 
-with open(new, 'rb') as f:
-    file_size = os.path.getsize(new)
-    if file_size <= CHUNK_SIZE:
-        dbx.files_upload(f.read(), '/' + new)
-    else:
-        upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
-        cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
-                                               offset=f.tell())
-        commit = dropbox.files.CommitInfo(path='/' + new)
+def upload_new_file(new, dbx):
+    with open(new, 'rb') as f:
+        file_size = os.path.getsize(new)
+        if file_size <= CHUNK_SIZE:
+            dbx.files_upload(f.read(), '/' + new)
+        else:
+            upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
+            cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
+                                                   offset=f.tell())
+            commit = dropbox.files.CommitInfo(path='/' + new)
 
-        while f.tell() < file_size:
-            try:
-                dbx.check_user()
-            except AuthError:
-                res = requests.post('https://api.dropbox.com/oauth2/token?grant_type=refresh_token&refresh_token=%s&client_id=%s&client_secret=%s' % (refresh_token, client_id, client_secret))
-                dbx = dropbox.Dropbox(res.json()['access_token'])
-            if ((file_size - f.tell()) <= CHUNK_SIZE):
-                print(dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
-                                                cursor,
-                                                commit))
-            else:
-                dbx.files_upload_session_append(f.read(CHUNK_SIZE),
-                                                cursor.session_id,
-                                                cursor.offset)
-                cursor.offset = f.tell()
+            while f.tell() < file_size:
+                try:
+                    dbx.check_user()
+                except AuthError:
+                    res = requests.post('https://api.dropbox.com/oauth2/token?grant_type=refresh_token&refresh_token=%s&client_id=%s&client_secret=%s' % (refresh_token, client_id, client_secret))
+                    dbx = dropbox.Dropbox(res.json()['access_token'])
+                if ((file_size - f.tell()) <= CHUNK_SIZE):
+                    print(dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
+                                                    cursor,
+                                                    commit))
+                else:
+                    try:
+                        dbx.files_upload_session_append(f.read(CHUNK_SIZE),
+                                                        cursor.session_id,
+                                                        cursor.offset)
+                    except ApiError:
+                        # Ensure file uploaded through recursion if fail
+                        upload_new_file(new, dbx)
+                    cursor.offset = f.tell()
+
+upload_new_file(new, dbx)
